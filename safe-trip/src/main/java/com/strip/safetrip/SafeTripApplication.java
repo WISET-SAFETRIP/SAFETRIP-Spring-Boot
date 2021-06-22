7640c7c251a92ff5a8dec1a5b185e78ce62fce19
Hee-jin506 <gmlwls35220@gmail.com>
2021-06-22 15:27:43 +0900

    first commit

---
+++ safe-trip/src/main/java/com/strip/safetrip/SafeTripApplication.java
@@ -0,0 +1,13 @@
+package com.strip.safetrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeTripApplication.class, args);
+	}
+
+}
---
+++ safe-trip/src/test/java/com/strip/safetrip/SafeTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strip.safetrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
